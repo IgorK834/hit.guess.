a1c24b261a08ccf4fb286db499f520686b970530
Igor <i.kadziela03@gmail.com>
2026-04-16 15:29:59 +0200

    chore: upgrade Next.js to 16 and React to 19
    Refresh frontend dependencies/tooling to the latest stable versions and add flat ESLint config required by the new stack.

---
+++ frontend/eslint.config.js
@@ -0,0 +1,6 @@
+/** @type {import('eslint').Linter.FlatConfig[]} */
+const nextConfig = require("eslint-config-next");
+
+// eslint-config-next already exports a flat-config array for Next.js.
+module.exports = nextConfig;
+
